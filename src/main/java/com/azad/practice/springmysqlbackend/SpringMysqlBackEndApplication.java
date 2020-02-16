9157634359fe5b8023c02c098784e97c05a72cd1
Azhar Uddin Azad <azhar.u.azad@gmail.com>
2020-02-16 16:32:25 +0600

    initial commit

---
+++ src/main/java/com/azad/practice/springmysqlbackend/SpringMysqlBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.azad.practice.springmysqlbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMysqlBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMysqlBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azad/practice/springmysqlbackend/SpringMysqlBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azad.practice.springmysqlbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMysqlBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
